7b2419bad38932c04036a0aef7a46138fc6ec13e
Joe Development <146123038+Joe-Development@users.noreply.github.com>
2024-02-22 06:04:20 -0500

    Create Config.lua

---
+++ Config.lua
@@ -0,0 +1,6 @@
+Config = {
+    holograms = {
+        { x = -428.85, y = 1151.0, z = 325.0, textInput = "~b~Welcome To ExampleRP \n ~r~ Hope You Have A Great Stay \n~p~ discord.gg/ExampleRP ", fontId = 0, scaleX = 0.1, scaleY = 0.1, viewDistance = 40.0 },
+        -- ^ Is at Observatory
+  }
+}
