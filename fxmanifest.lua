58bce68f780afb4fbe49376c3a547ea743166825
Joe Development <146123038+Joe-Development@users.noreply.github.com>
2024-02-22 06:04:44 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+games { 'gta5' }
+author 'JoeV2'
+description 'A FiveM Hologram System Made By JoeV2 To Enhance Your Server !'
+version '1.0.1'
+lua54 'true'
+
+client_scripts {
+    'Config.lua',
+    'hologram.lua',
+}
